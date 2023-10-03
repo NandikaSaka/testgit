078028bb00b8b3e50342da55d521701fc3857bd0
Made Nandika Saka Putra <115137019+NandikaSaka@users.noreply.github.com>
2023-10-03 10:29:29 +0800

    Delete 2205551006.2.php

--- 2205551006.2.php
+++
@@ -1,9 +0,0 @@
-<!-- menyimpan data kedalam variabel -->
-nim            = <?php echo $_POST['nim'];?><br>
-nama           = <?php echo $_POST['nama'];?><br>
-kelamin        = <?php echo $_POST['jenis_kelamin'];?><br>
-tgllahir       = <?php echo $_POST['thn'].'-'.$_POST['bln'].'-'.$_POST['tgl'];?><br>
-hobby          = <?php echo $_POST['hobby'];?><br>
-tmptlahir      = <?php echo $_POST['tmptlahir'];?><br>
-jurusan        = <?php echo $_POST['jurusan'];?><br>
-tahunmsk       = <?php echo $_POST['thnmsk'];?><br>
